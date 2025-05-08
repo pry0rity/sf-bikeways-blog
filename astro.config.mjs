37c0426e6d96d652a25158838e5e6011a4dbaf63
pry0rity <wmcmullen415@gmail.com>
2025-05-08 13:28:31 -0700

    added new dsn

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,21 @@ import sentry from '@sentry/astro';
 export default defineConfig({
   site: 'https://sf-bikeways-blog.vercel.app',
   output: 'static',
-  integrations: [tailwind(), mdx(), sitemap(), sentry()],
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    sitemap(),
+    sentry({
+      dsn: "https://4f58a81083674ee22bbdae75891b0667@o4508695687725056.ingest.us.sentry.io/4509289396764672",
+      tracesSampleRate: 1.0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 1.0,
+      sourceMapsUploadOptions: {
+        project: "pry0ritys-priorities",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    })
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
