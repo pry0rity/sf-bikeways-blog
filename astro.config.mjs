97fc53698f463e1f4b361f04412d7709d380db86
pry0rity <wmcmullen415@gmail.com>
2025-05-08 15:12:39 -0700

    new CSP policy for maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,11 +32,12 @@ export default defineConfig({
       headers: {
         'Content-Security-Policy': `
           default-src 'self';
-          script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.sentry.io;
-          connect-src 'self' https://*.sentry.io;
-          img-src 'self' data: https://*.sentry.io;
-          style-src 'self' 'unsafe-inline';
+          script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.sentry.io https://unpkg.com https://api.mapbox.com;
+          connect-src 'self' https://*.sentry.io https://*.tile.openstreetmap.org;
+          img-src 'self' data: https://*.sentry.io https://*.tile.openstreetmap.org https://unpkg.com;
+          style-src 'self' 'unsafe-inline' https://unpkg.com https://api.fontshare.com;
           frame-src 'self' https://open.spotify.com;
+          worker-src 'self' blob:;
         `.replace(/\s+/g, ' ').trim()
       }
     }
